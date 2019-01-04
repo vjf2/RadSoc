deb206704b9c3ecbfaeedc4128c0e0ab20539bca
Vivienne Foroughirad <foroughirad@gmail.com>
2019-01-03 21:22:42 -0500

    added confidence estimates

--- 5Sequoia_parentage.R
+++ 5Sequoia_parentage.R
@@ -41,14 +41,15 @@ lhl$father_id[which(lhl$dolphin_id=="PAS")]<-"HII"
 #load in plink raw file
 plink_raw <- "original2018p.raw"
 
-maf4loci <- GenoConvert(plink_raw, "raw")
+allloci <- GenoConvert(plink_raw, "raw")
 
 set.seed(1)
 
-ped_with_sibs <- sequoia(GenoM = maf4loci,
+ped_with_sibs <- sequoia(GenoM = allloci,
                               LifeHistData = LH_dolphins,
                               Err=0.01895,
-                              MaxMismatch = 30,
+                              Tassign = 0.5, # default 0.5
+                              Tfilter = -2, #default -2
                               MaxSibIter = 10, 
                               MaxSibshipSize = 20)
 
@@ -56,9 +57,7 @@ summary(ped_with_sibs)
 
 ped<-ped_with_sibs$Pedigree
 
-# SNPd=c(ID_key$Dolphin_ID[which(ID_key$me_paper=="yes")], "JUP", "ABC", "ANE", "ZUL", "NET", "APO", "FCH", "ORY","ATC", "GRV", "MNK", "WON", "HEL", "DUD", "UVE") #adding in UVE causes compare to fail sometimes
-
-SNPd=c(rownames(maf4loci))
+SNPd=c(rownames(allloci))
 
 compareOUT <- PedCompare(Ped1 = lhl[,c("dolphin_id", "mother_id", "father_id")], 
                          Ped2 = ped, na1="", SNPd=SNPd)
@@ -67,10 +66,6 @@ consensusped<-compareOUT$ConsensusPed
                          
 consensusped$sex<-lhl$sex[match(consensusped$id, lhl$dolphin_id)] 
 
-# newkin<-expected_kinship(mother_id = "dam",
-#                          father_id = "sire", sex = "sex", data=consensusped)
-
-                       
 # write.csv(ped, "sequoia_pedigree_output.csv")
 
 parented<-consensusped[which(consensusped$dam!="<NA>" & consensusped$sire!="<NA>" &
@@ -88,5 +83,10 @@ nmoms<-ped[ped$id %in% SNPd & !is.na(ped$dam),]
 
 compareOUT$P2only
 
+ConfPr<-EstConf(Ped = ped, LifeHistData = LH_dolphins, Specs = ped_with_sibs$Specs, Full = TRUE,
+        nSim = 20, ParMis = 0.6, args.sim = NULL, return.PC = TRUE,
+        quiet = TRUE)
+
+save(ConfPr, file="confrpr.RData")
 
 

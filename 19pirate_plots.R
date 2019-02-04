110ef92de638ac6b7f5de0eaab2085ef4681cdad
Vivienne Foroughirad <foroughirad@gmail.com>
2019-02-04 18:06:26 -0500

    adjusted figure 2 panel sizes

--- 19pirate_plots.R
+++ 19pirate_plots.R
@@ -20,6 +20,7 @@ pp<-function(df, xaxt=NULL, yaxt=NULL, nsnps=nsnps, ylim=ylim){
              xaxt = xaxt,
              yaxt = yaxt,
              ylim = ylim,
+             xlim = c(0.6, 5.4),
              gl.y = NA,
              bean.b.col=NA,
              bean.f.col=catcol,
@@ -39,36 +40,56 @@ pp<-function(df, xaxt=NULL, yaxt=NULL, nsnps=nsnps, ylim=ylim){
   
   points(x=rep(4.5,6), y=df$MEst[df$biparental==0.375], col=adjustcolor("black", alpha.f=0.2), pch=16, cex=0.75)
   
-  text(1, max(ylim)-.05, nsnps, cex=1.75)
+  text(1, max(ylim)-.05, nsnps, cex=1)
   
 }
 
-#remove boxes around plots?
 #will need to fix aspect ratios, etc. in final pdf plot
 
+lvec<-c(rep(1,57), 
+        rep(5, 43),
+        rep(2, 57), 
+        rep(6, 43), 
+        rep(3, 57), 
+        rep(7, 43), 
+        rep(4, 57), 
+        rep(8, 43))
+
+lmat<-matrix(lvec, nrow=100, ncol=4)
+
 windows()
-#pdf(file="Figures/pirateplot.pdf", width=16, height=9)
-par(mfrow=c(2,4), mar=c(0,0,0,0), oma=c(6,6,1,1))
+#pdf(file="Figures/Figure_2.pdf", width=6.65354, height=4.132634)
+par(mar=c(0,0,0,0), oma=c(4,3.5,0.5,0.1))
+
+layout(lmat)
 
 pp(ped_est[[7]], "n", "n", nsnps=50, ylim=c(-0.02, 0.81))
-axis(2, las=1, at=c(0,0.1,0.2,0.3,0.4,0.5, 0.6, 0.7, 0.8), cex.axis=1.5)
+axis(2, las=1, at=c(0,0.1,0.2,0.3,0.4,0.5, 0.6, 0.7, 0.8), cex.axis=0.8, srt=45, xpd=TRUE)
 pp(ped_est[[1]], "n", "n", nsnps=100, ylim=c(-0.02, 0.81))
 pp(ped_est[[3]], "n", "n", nsnps=200, ylim=c(-0.02, 0.81))
 pp(ped_est[[5]], "n", "n", nsnps=400, ylim=c(-0.02, 0.81))
 pp(ped_est[[8]], "n", "n", nsnps=800, ylim=c(-0.02, 0.61))
-axis(1, at=1:5, labels=c(0, 0.0625, 0.125, 0.25, 0.5), cex.axis=1.5)
-axis(2, las=1, at=c(0,0.1,0.2,0.3,0.4,0.5, 0.6), cex.axis=1.5)
+axis(1, at=c(1,3,5), labels=c(0,0.125,0.5), cex.axis=0.8)
+axis(1, at=c(2,4), labels=c(0.0625,0.25), cex.axis=0.8)
+axis(2, las=1, at=c(0,0.1,0.2,0.3,0.4,0.5, 0.6), cex.axis=0.8)
 pp(ped_est[[2]], "n", yaxt="n", nsnps=1600, ylim=c(-0.02, 0.61))
-axis(1, at=1:5, labels=c(0, 0.0625, 0.125, 0.25, 0.5), cex.axis=1.5)
+axis(1, at=c(1,3,5), labels=c(0,0.125,0.5), cex.axis=0.8)
+axis(1, at=c(2,4), labels=c(0.0625,0.25), cex.axis=0.8)
+
 pp(ped_est[[4]], "n", yaxt="n", nsnps=3200, ylim=c(-0.02, 0.61))
-axis(1, at=1:5, labels=c(0, 0.0625, 0.125, 0.25, 0.5), cex.axis=1.5)
+axis(1, at=c(1,3,5), labels=c(0,0.125,0.5), cex.axis=0.8)
+axis(1, at=c(2,4), labels=c(0.0625,0.25), cex.axis=0.8)
+
 pp(ped_est[[6]], "n", yaxt="n", nsnps=4235, ylim=c(-0.02, 0.61))
-axis(1, at=1:5, labels=c(0, 0.0625, 0.125, 0.25, 0.5), cex.axis=1.5)
+axis(1, at=c(1,3,5), labels=c(0,0.125,0.5), cex.axis=0.8)
+axis(1, at=c(2,4), labels=c(0.0625,0.25), cex.axis=0.8)
 
 
-mtext("Pedigree Relatedness",1, outer=TRUE, line=4, cex=1.25)
-mtext("Genetic Relatedness",2, outer=TRUE, line=4, cex=1.25)
+mtext("Pedigree Relatedness",1, outer=TRUE, line=2.5, cex=0.75)
+mtext("Genetic Relatedness",2, outer=TRUE, line=2.5, cex=0.75)
 
 dev.off()
+
+
 
 
